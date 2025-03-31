15d26f77f90b8c8d195354994654494ee2ae2fe3
NetroTypeShi <andresmenduina@gmail.com>
2025-03-31 22:13:45 +0200

    break system attemp

--- Assets/Scripts/VehicleMovement.cs
+++ Assets/Scripts/VehicleMovement.cs
@@ -6,11 +6,16 @@ using UnityEngine;
 public class VehicleMovement : MonoBehaviour
 {
     Rigidbody rb;
-    [SerializeField] float speed;
-    [SerializeField] float forceDirection;
+    [SerializeField] float acceleration;
+    [SerializeField] float turnForce;
+    [SerializeField] float brakeForce;
+    [SerializeField] float maxSpeed;
+    float forwardSpeed;
     bool forward = false;
     bool reverse = false;
     Vector3 vehicleDirection;
+    
+    int giro = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,59 +23,30 @@ public class VehicleMovement : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        rb.velocity += vehicleDirection * speed * Time.fixedDeltaTime;
+        
+
         vehicleDirection = Vector3.zero;
-        rb.AddTorque(0, forceDirection, 0);
+        forwardSpeed = Vector3.Dot(rb.velocity, transform.forward);
         if (forward == true)
         {
-            vehicleDirection = Vector3.forward;
+            vehicleDirection = gameObject.transform.forward;
         }
         if (reverse == true)
         {
-            vehicleDirection = Vector3.back;
+            vehicleDirection = -gameObject.transform.forward;           
         }
+
+        if (rb.velocity.magnitude <= maxSpeed) { rb.velocity += vehicleDirection * acceleration * Time.fixedDeltaTime; }
+
+        rb.AddTorque(0, turnForce*giro, 0);
     }
     void Update()
     {
-        forceDirection = 0;
-        if (Input.GetKey(KeyCode.W))
-        {
-            forward = true;
-        }
-        else
-        {
-            forward = false;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            reverse = true;
-        }
-        else
-        {
-            reverse = false;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            forceDirection = 300;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            forceDirection = -300;
-        }
+        giro = 0;
+        forward = Input.GetKey(KeyCode.W);
+        if (Input.GetKey(KeyCode.S) && rb.velocity.magnitude > 0) {}
+        if (Input.GetKey(KeyCode.A)){giro = -1;}
+        if (Input.GetKey(KeyCode.D)){giro = 1;}
+        if (Input.GetKey(KeyCode.D)&& Input.GetKey(KeyCode.A)) { giro = 0; }
     }
 }
-/*
- * 
- * 
- * acelerar:
- *      pulsarla
- *      no pulsarla
- *      
- * giro:
- *      no pulsar na || pulsar ambas
- *      pulsar izq
- *      pulsar der
- *      
- * 
- * 
- */
